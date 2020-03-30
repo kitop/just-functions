0df6d8b09b9a55f138d63e42a6ce58e983c5e4ad
Esteban Pastorino <ejpastorino@gmail.com>
2020-03-30 12:41:02 +0100

    updates

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "Hello, I'm just a function, alone in this site :'("
+    body: "Hello, I'm just a function, alone in this site :'( :'("
   });
 };
