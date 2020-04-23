ef173a5539596ad5edc7d35eb5550615ff6fdac4
Esteban Pastorino <ejpastorino@gmail.com>
2020-04-23 12:08:20 +0100

    updates

--- functions/goodbye.js
+++ functions/goodbye.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "The other function is a liar, it's not alone."
+    body: "The other function is a liar, it's not alone. I am here too."
   });
 };
